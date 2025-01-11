450b96cae04bc7aacd0e254f2e19af56048400bf
Rajan <krajan92946@gmail.com>
2025-01-11 15:25:15 +0530

    Login controller done

--- LOGIN-REGISTRATION/backend/Routes/UserRoutes.js
+++ LOGIN-REGISTRATION/backend/Routes/UserRoutes.js
@@ -5,7 +5,7 @@ const route=express.Router();
 const UserController=require("../Controllers/UserController")
 
 route.post("/registration",UserController.Registration)
-route.post("/userlogin", UserController.Login)
+
 
 
 module.exports=route;
